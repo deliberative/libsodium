58d85a0a79be7946420ae8f238bb2c260b2e57fd
fuzzc0re <dim@karoukis.com>
2022-08-11 11:17:12 +0300

    Renamed G in blake2b for argon2

--- src/libsodium/crypto_pwhash/argon2/blamka-round-ref.h
+++ src/libsodium/crypto_pwhash/argon2/blamka-round-ref.h
@@ -12,7 +12,7 @@ fBlaMka(uint64_t x, uint64_t y)
     return x + y + 2 * xy;
 }
 
-#define G(a, b, c, d)          \
+#define BLAMKA_G(a, b, c, d)          \
     do {                       \
         a = fBlaMka(a, b);     \
         d = ROTR64(d ^ a, 32); \
@@ -27,14 +27,14 @@ fBlaMka(uint64_t x, uint64_t y)
 #define BLAKE2_ROUND_NOMSG(v0, v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, \
                            v12, v13, v14, v15)                               \
     do {                                                                     \
-        G(v0, v4, v8, v12);                                                  \
-        G(v1, v5, v9, v13);                                                  \
-        G(v2, v6, v10, v14);                                                 \
-        G(v3, v7, v11, v15);                                                 \
-        G(v0, v5, v10, v15);                                                 \
-        G(v1, v6, v11, v12);                                                 \
-        G(v2, v7, v8, v13);                                                  \
-        G(v3, v4, v9, v14);                                                  \
+        BLAMKA_G(v0, v4, v8, v12);                                           \
+        BLAMKA_G(v1, v5, v9, v13);                                           \
+        BLAMKA_G(v2, v6, v10, v14);                                          \
+        BLAMKA_G(v3, v7, v11, v15);                                          \
+        BLAMKA_G(v0, v5, v10, v15);                                          \
+        BLAMKA_G(v1, v6, v11, v12);                                          \
+        BLAMKA_G(v2, v7, v8, v13);                                           \
+        BLAMKA_G(v3, v4, v9, v14);                                           \
     } while ((void) 0, 0)
 
 #endif
